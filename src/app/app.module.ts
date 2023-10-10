7e42b535a060add4d09af3cb78c32470b3dcd8dc
AgusMott <motturaagustin@gmail.com>
2023-10-09 22:12:29 -0300

    haleluya

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListaComponent } from './lista/lista.component';
+import { InfoComponent } from './info/info.component';
+import { LoginComponent } from './home/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListaComponent,
+    InfoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
